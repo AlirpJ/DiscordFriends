c62d07f35662d55e274e28a2b6cac63d3f3ab9fe
Joshua Prila <prila.jsp@gmail.com>
2023-05-08 10:07:45 -0500

    Create friend.js

---
+++ friend.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const {Client, RichEmbed, DiscordAPIError } = require('discord.js')
+const bot = new Client()
+
+const PREFIX = '!'
